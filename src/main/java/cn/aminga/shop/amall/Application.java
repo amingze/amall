64d88695761a251bbab29f8ff6229448179145cb
amingze <kailynnsn2@gmail.com>
2020-04-29 12:43:38 +0800

    create Application

---
+++ src/main/java/cn/aminga/shop/amall/Application.java
@@ -0,0 +1,20 @@
+package cn.aminga.shop.amall;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class Application {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args)   {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
